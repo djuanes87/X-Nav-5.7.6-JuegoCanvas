328b85f8ab095446131482d4b971e26d833a5e80
Daniel Juanes <d.juanes@alumnos.urjc.es>
2016-02-25 20:21:09 +0100

    El heroe no puede salir del recinto arbolado

--- js/game.js
+++ js/game.js
@@ -65,16 +65,24 @@ var reset = function () {
 // Update game objects
 var update = function (modifier) {
 	if (38 in keysDown) { // Player holding up
-		hero.y -= hero.speed * modifier;
+		if(hero.y >=(32)){ //Impide que salga por arriba
+			hero.y -= hero.speed * modifier;
+		}
 	}
 	if (40 in keysDown) { // Player holding down
-		hero.y += hero.speed * modifier;
+		if(hero.y <=((canvas.height-64))){ //Impide que salga por abajo
+			hero.y += hero.speed * modifier;
+		}
 	}
 	if (37 in keysDown) { // Player holding left
-		hero.x -= hero.speed * modifier;
+		if(hero.x >= 32){//Impide salir por la izquierda
+			hero.x -= hero.speed * modifier;
+		}
 	}
 	if (39 in keysDown) { // Player holding right
-		hero.x += hero.speed * modifier;
+		if(hero.x <= ((canvas.width - 64))){ //impide salir por la derecha
+			hero.x += hero.speed * modifier;
+		}
 	}
 
 	// Are they touching?
